c794f91a86e48856e8bf20ed09dc75725313ffee
Paulemeister <annanas6800i@gmail.com>
2021-05-21 18:55:17 +0200

    fixed typo

--- B.4.13.2/test.cpp
+++ B.4.13.2/test.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
     C1DPunkt p1 = C1DPunkt(1);
-    C1DPunkt p2 = C2DPunkt(2, 3);
-    C1DPunkt p3 = C3DPunkt(4, 5, 6);
+    C2DPunkt p2 = C2DPunkt(2, 3);
+    C3DPunkt p3 = C3DPunkt(4, 5, 6);
     punkt_ausgeben(&p1);
     punkt_ausgeben(&p2);
     punkt_ausgeben(&p3);
